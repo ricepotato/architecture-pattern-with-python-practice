ce9458a05be004f90c4eb6ae3b0a80df3b8a6267
ricepotato <ricepotato40@gmail.com>
2021-12-20 02:13:55 +0900

    repository pattern

--- repository.py
+++ repository.py
@@ -10,3 +10,31 @@ class AbstractRepository(abc.ABC):
     @abc.abstractmethod
     def get(self, reference) -> model.Batch:
         raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        return self.session.query(model.Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(model.Batch).all()
+
+
+class FakeRepository(AbstractRepository):
+    def __init__(self, batches):
+        self._batches = set(batches)
+
+    def add(self, batch):
+        self._batches.add(batch)
+
+    def get(self, reference):
+        return next(b for b in self._batches if b.reference == reference)
+
+    def list(self):
+        return list(self._batches)
---
+++ test_orm.py
@@ -0,0 +1,19 @@
+import model
+
+
+def test_orderline_mapper_can_load_lines(session):
+    session.execute(
+        "INSERT INTO order_lines (orderid, sku, qty) VALUES "
+        '("order1", "RED-CHAIR", 12),'
+        '("order1", "RED-CHAIR", 13),'
+        '("order2", "BLUE_LIPSTICK", 14),'
+    )
+
+
+def test_orderline_mapper_can_save_lines(session):
+    new_line = model.OrderLine("order1", "DECORATIVE-WIDGET", 12)
+    session.add(new_line)
+    session.commit()
+
+    rows = list(session.execute('SELECT orderid, sku, qty FROM "order_lines"'))
+    assert rows == [("order1", "DECORATIVE-WIDGET", 12)]
