959fd173acd3e038814967c2f1c8b1f77c5d437f
Sukjun Sagong <ricepotato40@gmail.com>
2021-12-20 19:20:55 +0900

    update test_orm

--- test_orm.py
+++ test_orm.py
@@ -5,30 +5,19 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from orm import metadata, start_mappers
 
 
-start_mappers()
-
-
-@pytest.fixture
-def session():
-
-    engine = create_engine("sqlite://")
-    metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    yield session
-
-    metadata.drop_all(engine)
-    session.close()
-
-
 def test_orderline_mapper_can_load_lines(session):
     session.execute(
         "INSERT INTO order_lines (orderid, sku, qty) VALUES "
         '("order1", "RED-CHAIR", 12),'
-        '("order1", "RED-CHAIR", 13),'
-        '("order2", "BLUE_LIPSTICK", 14)'
+        '("order1", "RED-TABLE", 13),'
+        '("order2", "BLUE-LIPSTICK", 14)'
     )
+    expected = [
+        model.OrderLine("order1", "RED-CHAIR", 12),
+        model.OrderLine("order1", "RED-TABLE", 13),
+        model.OrderLine("order2", "BLUE-LIPSTICK", 14),
+    ]
+    assert session.query(model.OrderLine).all() == expected
 
 
 def test_orderline_mapper_can_save_lines(session):
