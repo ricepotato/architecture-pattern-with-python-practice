bdc2898e219aea5129d1933c77796e42d13f1687
Sukjun Sagong <ricepotato40@gmail.com>
2021-12-22 19:24:48 +0900

    fix: db 오류 수정

--- model.py
+++ model.py
@@ -1,15 +1,25 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from datetime import date
-from typing import Optional, List
+from typing import Optional, List, Set
 
 
 class OutOfStock(Exception):
     pass
 
 
-@dataclass(frozen=True)
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    try:
+        batch = next(b for b in sorted(batches) if b.can_allocate(line))
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f"Out of stock for sku {line.sku}")
+
+
+@dataclass(unsafe_hash=True)
 class OrderLine:
-    order_id: str
+    orderid: str
     sku: str
     qty: int
 
@@ -20,26 +30,10 @@ class Batch:
         self.sku = sku
         self.eta = eta
         self._purchased_quantity = qty
-        self._allocations = set()
+        self._allocations = set()  # type: Set[OrderLine]
 
-    def allocate(self, line: OrderLine):
-        if self.can_allocate(line):
-            self._allocations.add(line)
-
-    def deallocate(self, line: OrderLine):
-        if line in self._allocations:
-            self._allocations.remove(line)
-
-    def can_allocate(self, line: OrderLine) -> bool:
-        return self.sku == line.sku and self.available_quantity >= line.qty
-
-    @property
-    def allocated_quantity(self) -> int:
-        return sum(line.qty for line in self._allocations)
-
-    @property
-    def available_quantity(self) -> int:
-        return self._purchased_quantity - self.allocated_quantity
+    def __repr__(self):
+        return f"<Batch {self.reference}>"
 
     def __eq__(self, other):
         if not isinstance(other, Batch):
@@ -56,11 +50,21 @@ class Batch:
             return True
         return self.eta > other.eta
 
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
 
-def allocate(line: OrderLine, batches: List[Batch]) -> str:
-    try:
-        batch = next(b for b in sorted(batches) if b.can_allocate(line))
-        batch.allocate(line)
-        return batch.reference
-    except StopIteration:
-        raise OutOfStock(f"Out of stock for sku {line.sku}")
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
--- test_repository.py
+++ test_repository.py
@@ -5,41 +5,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from orm import metadata, start_mappers
 
-start_mappers()
-
-
-@pytest.fixture
-def session():
-
-    engine = create_engine("sqlite://")
-    metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    yield session
-
-    metadata.drop_all(engine)
-    session.close()
-
 
 def test_repository_can_save_a_batch(session):
-    batch = model.Batch("batch1", "RUSTRY-SOAPDISH", 100, eta=None)
+    batch = model.Batch("batch1", "RUSTY-SOAPDISH", 100, eta=None)
 
     repo = repository.SqlAlchemyRepository(session)
     repo.add(batch)
     session.commit()
 
-    rows = list(
-        session.execute(
-            'SELECT reference, sku, _purchased_quantity, eta FROM "batches"'
-        )
+    rows = session.execute(
+        'SELECT reference, sku, _purchased_quantity, eta FROM "batches"'
     )
-    assert rows == [("batch1", "RUSTY-SOAPDISH", 100, None)]
+    assert list(rows) == [("batch1", "RUSTY-SOAPDISH", 100, None)]
 
 
 def insert_order_line(session):
     session.execute(
-        "INSERT INFO order_lines (orderid, sku, qty)"
+        "INSERT INTO order_lines (orderid, sku, qty)"
         ' VALUES ("order1", "GENERIC-SOFA", 12)'
     )
     [[orderline_id]] = session.execute(
@@ -70,17 +52,19 @@ def insert_allocation(session, orderline_id, batch_id):
     )
 
 
-def test_repository_can_retrive_a_batch_with_allocations(session):
+def test_repository_can_retrieve_a_batch_with_allocations(session):
     orderline_id = insert_order_line(session)
     batch1_id = insert_batch(session, "batch1")
     insert_batch(session, "batch2")
     insert_allocation(session, orderline_id, batch1_id)
 
     repo = repository.SqlAlchemyRepository(session)
-    retrived = repo.get("batch1")
+    retrieved = repo.get("batch1")
 
     expected = model.Batch("batch1", "GENERIC-SOFA", 100, eta=None)
-    assert retrived == expected
-    assert retrived.sku == expected.sku
-    assert retrived._purchased_quantity == expected._purchased_quantity
-    assert retrived._allocations == {model.OrderLine("order1", "GENERIC-SOFA", 12)}
+    assert retrieved == expected  # Batch.__eq__ only compares reference
+    assert retrieved.sku == expected.sku
+    assert retrieved._purchased_quantity == expected._purchased_quantity
+    assert retrieved._allocations == {
+        model.OrderLine("order1", "GENERIC-SOFA", 12),
+    }
