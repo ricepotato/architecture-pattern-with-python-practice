594441f68c3fcb7ca2cdcc96db869c23cc3f6a82
ricepotato <ricepotato40@gmail.com>
2021-12-28 00:38:32 +0900

    update test_services

--- test_services.py
+++ test_services.py
@@ -1,4 +1,5 @@
 import model
+import pytest
 from repository import FakeRepository
 import services
 
@@ -17,3 +18,12 @@ def test_returns_allocation():
 
     result = services.allocate(line, repo, FakeSession())
     assert result == "b1"
+
+
+def test_error_for_invalid_sku():
+    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
+    batch = model.Batch("b1", "AREALSKU", 100, eta=None)
+    repo = FakeRepository([batch])
+
+    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+        services.allocate(line, repo, FakeSession())
