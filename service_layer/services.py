96ba5d2a00ef791d11f62679234b037b9bf94e9a
ricepotato <sukjun40@naver.com>
2022-01-12 01:06:17 +0900

    service layer test, line 의존성 테스트에서 제거

--- service_layer/services.py
+++ service_layer/services.py
@@ -10,7 +10,10 @@ def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-def allocate(line: model.OrderLine, repo: AbstractRepository, session) -> str:
+def allocate(
+    orderid: str, sku: str, qty: int, repo: AbstractRepository, session
+) -> str:
+    line = model.OrderLine(orderid, sku, qty)
     batches = repo.list()
     if not is_valid_sku(line.sku, batches):
         raise InvalidSku(f"Invalid sku {line.sku}")
--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,3 +1,4 @@
+import datetime
 import domain.model as model
 import pytest
 from adapters.repository import FakeRepository
@@ -12,18 +13,46 @@ class FakeSession:
 
 
 def test_returns_allocation():
-    line = model.OrderLine("o1", "COMPLICATAED-LAMP", 10)
+    # line = model.OrderLine("o1", "COMPLICATAED-LAMP", 10)
     batch = model.Batch("b1", "COMPLICATAED-LAMP", 100, eta=None)
     repo = FakeRepository([batch])
 
-    result = services.allocate(line, repo, FakeSession())
+    result = services.allocate("o1", "COMPLICATAED-LAMP", 10, repo, FakeSession())
     assert result == "b1"
 
 
 def test_error_for_invalid_sku():
-    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
+    # line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
     batch = model.Batch("b1", "AREALSKU", 100, eta=None)
     repo = FakeRepository([batch])
 
     with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
-        services.allocate(line, repo, FakeSession())
+        services.allocate("o1", "NONEXISTENTSKU", 10, repo, FakeSession())
+
+
+def test_prefers_current_stock_batches_to_shipments():
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    in_stock_batch = model.Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = model.Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+
+    line = model.OrderLine("oref", "RETRO-CLOCK", 10)
+
+    model.allocate(line, [in_stock_batch, shipment_batch])
+
+    assert in_stock_batch.available_quantity == 90
+    assert shipment_batch.available_quantity == 100
+
+
+def test_prefers_warehouse_batches_to_shipments():
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    in_stock_batch = model.Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = model.Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+
+    repo = FakeRepository([in_stock_batch, shipment_batch])
+    session = FakeSession()
+
+    # line = model.OrderLine("oref", "RETRO-CLOCK", 10)
+    services.allocate("oref", "RETRO-CLOCK", 10, repo, session)
+
+    assert in_stock_batch.available_quantity == 90
+    assert shipment_batch.available_quantity == 100
