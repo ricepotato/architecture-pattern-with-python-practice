fd4b13df66d9881b9e984f30e28bfec9d77b3b6d
ricepotato <sukjun40@naver.com>
2022-01-28 23:44:36 +0900

    command, event

--- service_layer/messagebus.py
+++ service_layer/messagebus.py
@@ -1,4 +1,7 @@
-from domain import events
+import logging
+from typing import Union, List
+from tenacity import Retrying, RetryError, stop_after_attempt, wait_exponential
+from domain import events, commands
 from service_layer import unit_of_work
 from service_layer.handler import (
     change_batch_quantity,
@@ -7,21 +10,67 @@ from service_layer.handler import (
     allocate,
 )
 
+logger = logging.getLogger("app")
 
-def handle(event: events.Event, uow: unit_of_work.AbstractUnitOfWork):
+
+Message = Union[commands.Command, events.Event]
+
+
+def handle(message: Message, uow: unit_of_work.AbstractUnitOfWork):
     results = []
-    queue = [event]
+    queue = [message]
     while queue:
-        event = queue.pop(0)
-        for handler in HANDLERS[type(event)]:
-            results.append(handler(event, uow=uow))
-            queue.extend(uow.collect_new_events())
+        message = queue.pop(0)
+        if isinstance(message, events.Event):
+            handle_event(message, queue, uow)
+        elif isinstance(message, commands.Command):
+            cmd_result = handle_command(message, queue, uow)
+            results.append(cmd_result)
+        else:
+            raise Exception(f"{message} was not a event or commnad")
     return results
 
 
-HANDLERS = {
+EVENT_HANDLERS = {
     events.OutOfStock: [send_out_of_stock_notification],
-    events.BatchCreated: [add_batch],
-    events.AllocationRequired: [allocate],
-    events.BatchQuantityChanged: [change_batch_quantity],
 }
+
+COMMAND_HANDLERS = {
+    commands.Allocate: allocate,
+    commands.CreateBatch: add_batch,
+    commands.ChangeBatchQuantity: change_batch_quantity,
+}
+
+
+def handle_event(
+    event: events.Event, queue: List[Message], uow: unit_of_work.AbstractUnitOfWork
+):
+    for handler in EVENT_HANDLERS[type(event)]:
+        try:
+            for attempt in Retrying(
+                stop=stop_after_attempt(3), wait=wait_exponential()
+            ):
+                with attempt:
+                    logger.debug("handle event %s with handler %s", event, handler)
+                    handler(event, uow=uow)
+                    queue.extend(uow.collect_new_events())
+        except RetryError as retry_failure:
+            logger.error(
+                "failed to handle event %s times, giving up!",
+                retry_failure.last_attempt.attempt_number,
+            )
+            continue
+
+
+def handle_command(
+    command: events.Event, queue: List[Message], uow: unit_of_work.AbstractUnitOfWork
+):
+    logger.debug("handing command %s", command)
+    try:
+        handler = COMMAND_HANDLERS[type(command)]
+        result = handler(command, uow=uow)
+        queue.extend(uow.collect_new_events())
+        return result
+    except Exception:
+        logger.exception("exception handing command %s", command)
+        raise
